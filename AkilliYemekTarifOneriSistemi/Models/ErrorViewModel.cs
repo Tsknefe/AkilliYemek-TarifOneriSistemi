9c43755ab96958fadab03df6f30d84e1fd61ae41
tsknefe <taskiranefe5@gmail.com>
2025-10-05 03:02:35 +0300

    chore: initial commit (MVC setup + gitignore)

---
+++ AkilliYemekTarifOneriSistemi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AkilliYemekTarifOneriSistemi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
